5629885208e8db86d0332d321c5a1c501225b0e4
SLMeri <74334648+SLMeri@users.noreply.github.com>
2022-01-19 08:12:00 -0800

    Workout Class with workout generator

--- src/com/spartan/workout/competition/client/CompetitionDriver.java
+++
@@ -1,8 +0,0 @@
-package com.spartan.workout.competition.client;
-
-public class CompetitionDriver {
-
-    public static void main(String[] args) {
-        System.out.println("WELCOME TO THE NEW SPARTAN WARRIOR COMPETITON!!");
-    }
-}
