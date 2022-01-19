43028738f98db85958a078948dd3abbf53302557
Vernon Stephens <97752165+Blackxican93@users.noreply.github.com>
2022-01-19 17:23:48 -0600

    End of day updates
    Updates to Team and TimeKeeper

--- src/com/spartan/competition/Team.java
+++ src/com/spartan/competition/Team.java
@@ -1,14 +1,23 @@
 package com.spartan.competition;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 
 public class Team {
+    private List<Person>team;
+
     public static void main(String[] args) {
 
+
+        //Team List
+        List<String> teams = new ArrayList<>();
+        teams.add("Team 1");
+        teams.add("Team 2");
+        teams.add("Team 3");
+        teams.add("Team 4");
+        teams.add("Team 5");
+        //System.out.println("Team list " + teams.toString());
         //Workout List
         List<String> workouts = new ArrayList<>();
         workouts.add("Workout 1");
@@ -16,23 +25,35 @@ public class Team {
         workouts.add("Workout 3");
         workouts.add("Workout 4");
         workouts.add("Workout 5");
-        System.out.println("Workout list " + workouts.toString());
-        //Team List
-        List<String> teams = new ArrayList<>();
-        teams.add("Team 1 = Members 1, 2, 3");
-        teams.add("Team 2 = Members 4, 5, 6");
-        teams.add("Team 3 = Members 7, 8, 9");
-        teams.add("Team 4 = Members 10, 11, 12");
-        teams.add("Team 5 = Members 13, 14, 15");
-        System.out.println("Team list " + teams.toString());
+        //System.out.println("Workout list " + workouts.toString());
+        //Members List
+        List<String> members = new ArrayList<>();
+        members.add("Members 1, 2, 3");
+        members.add("Members 4, 5, 6");
+        members.add("Members 7, 8, 9");
+        members.add("Members 10, 11, 12");
+        members.add("Members 13, 14, 15");
 
-        for (int i = 0; i < teams.size(); i++) {
-           int index = (int) (Math.random() * teams.size());
-           System.out.println(" Random Element is :" + teams.get(index));
-       }
+        Random numberGenerator = new Random();
+        int nextRandom = numberGenerator.nextInt(5);
+        Set<Integer> validate = new HashSet<>();
+        validate.add(nextRandom);
+        for (int i = 0; i < 5; i++) {
+            while(validate.contains(nextRandom)) {
+                nextRandom = numberGenerator.nextInt(5);
+            }
+            validate.add(nextRandom);
+            System.out.println(teams.get(nextRandom));
+            System.out.println(workouts.get(nextRandom));
+            System.out.println(members.get(nextRandom));
+        }
     }
 
 }
+
+        //for (int i = 0; i < teams.size(); i++) {
+        //int index = (int) (Math.random() * teams.size());
+        //System.out.println(" Random Element is :" + teams.get(index));
 
 //       for (int i = 0; i < teams.size(); i++) {
 //           int index = (int) (Math.random() * teams.size());
--- src/com/spartan/competition/TimeKeeper.java
+++ src/com/spartan/competition/TimeKeeper.java
@@ -1,4 +1,6 @@
-package com.spartan.competition.client;
+package com.spartan.competition;
+
+import java.util.Scanner;
 
 public class TimeKeeper {
     private long startTimer;
@@ -6,6 +8,9 @@ public class TimeKeeper {
     private boolean runningTimer;
     private boolean pausedTimer;
     private long pausedStart;
+    private double times[];
+    private double range;
+    private int totalPoints;
 
     public TimeKeeper() {
         this.pausedStart = 0;
@@ -16,47 +21,85 @@ public class TimeKeeper {
     public boolean isRunningTimer() {
         return runningTimer;
     }
+
     public boolean isPausedTimer() {
         return pausedTimer;
     }
+
     public void startTimer() {
         startTimer = System.nanoTime();
         runningTimer = true;
         pausedTimer = false;
         pausedStart = -1;
     }
+
     public long stopTimer() {
         if (!isRunningTimer()) {
             return -1;
+
         } else if (isPausedTimer()) {
             runningTimer = false;
             pausedTimer = false;
-
             return pausedStart - startTimer;
+
         } else {
             endTimer = System.nanoTime();
             runningTimer = false;
             return endTimer - startTimer;
         }
     }
+
     public long pauseTimer() {
-        if(!isRunningTimer()) {
+        if (!isRunningTimer()) {
             return -1;
-    }   else if (isPausedTimer()) {
+        } else if (isPausedTimer()) {
             return (pausedStart - startTimer);
-    } else {
+        } else {
             pausedStart = System.nanoTime();
             pausedTimer = true;
             return (pausedStart - startTimer);
         }
     }
+
     public long elapsed() {
         if (isRunningTimer()) {
-            if (isPausedTimer())
+            if (isPausedTimer()) {
                 return (pausedStart - startTimer);
- //           return (System.nanoTime()) - startTimer);
-        } else
+            }
+            return (System.nanoTime() - startTimer);
+        } else {
             return (endTimer - startTimer);
+        }
+    }
+
+    public void readTimes() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the race times (in minutes):");
+        for(int i = 0; i < 3; i++) {
+            times[i] = sc.nextDouble();
+        }
+        sortTimesAscending();
+        range = times[2] - times[0];
+
+    }
+
+    private void sortTimesAscending() {
+        double temp;
+        for(int i =0; i<3;i++){
+            for(int j = 0; j<3;j++) {
+                if(this.times[i] < this.times[j]) {
+                    temp = this.times[i];
+                    this.times[i] = this.times[j];
+                    this.times[j] = temp;
+                }
+            }
+        }
+    }
+
+    public void totalPoints() {
+        System.out.println("First place (time in minutes):" + times[2]);
+        System.out.println("Second place (time in minutes):" + times[1]);
+        System.out.println("Third place (time in minutes):" + times[0]);
     }
 
     public String toString() {
